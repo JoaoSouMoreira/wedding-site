51b84c789889dc77a62e9656af5f03e1162fc494
Joao Moreira <joao.moreira@funeralzone.co.uk>
2019-09-15 15:37:18 +0100

    Update fotos page

--- src/pages/fotos/index.js
+++ src/pages/fotos/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Gallery from 'react-grid-gallery';
 
 import BasePage from '../base';
 import NavigationBar from '../../client/components/navbar';
@@ -10,13 +11,32 @@ import baseStyles from '../home/home.css';
 
 class FotosPage extends BasePage {
 
+    getImages() {
+        const images = [];
+        for (let i = 0; i < 12; i++) {
+            images.push({
+                src: '/assets/fotos' + (i + 1) + '.jpg',
+                thumbnail: '/assets/fotos' + (i + 1) + '.jpg',
+                thumbnailWidth: 320,
+                thummbnailHeight: 174,
+                isSelected: false,
+            });
+        }
+        return images;
+    }
+
     render() {
         return <div className={baseStyles.main}>
             <WeddingHeader  setLanguage={this.setLanguage.bind(this)}></WeddingHeader>
             <div className={baseStyles.container}>
                 <NavigationBar path={this.props.location.pathname}></NavigationBar>
                 <div className={baseStyles.content}>
-                    <h1>{strings.fotos1}</h1>
+                    <h2>
+                        {strings.fotos2}
+                        <a href="https://luisastarling.pixieset.com/casamentoisabelandjoo" target="_blank">{strings.fotos3}</a>
+                    </h2>
+                    <h2>{strings.fotos1}</h2>
+                    <Gallery images={this.getImages()} />
                 </div>
             </div>
             <WeddingFooter></WeddingFooter>
